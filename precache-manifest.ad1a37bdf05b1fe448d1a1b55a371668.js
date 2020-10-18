cc28392c117d9b8ebb8fdbf02e603220bac3c8ce
Kiselov Vadym <vkiselyov@mindk.com>
2020-10-18 13:48:43 +0300

    Updates

---
+++ precache-manifest.ad1a37bdf05b1fe448d1a1b55a371668.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "170ac0f0f8aae1bfa23ac0780c35d3f0",
+    "url": "/image-manager/index.html"
+  },
+  {
+    "revision": "a457bbd646e04accf61e",
+    "url": "/image-manager/static/css/2.45eefbab.chunk.css"
+  },
+  {
+    "revision": "5498a119debeb8bf12dd",
+    "url": "/image-manager/static/css/main.ce879ad1.chunk.css"
+  },
+  {
+    "revision": "a457bbd646e04accf61e",
+    "url": "/image-manager/static/js/2.e5a96ff8.chunk.js"
+  },
+  {
+    "revision": "1937a0fc6634269e5f47f8181e27fdc7",
+    "url": "/image-manager/static/js/2.e5a96ff8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5498a119debeb8bf12dd",
+    "url": "/image-manager/static/js/main.1ba73646.chunk.js"
+  },
+  {
+    "revision": "94fa32a0f7d9666081ea",
+    "url": "/image-manager/static/js/runtime-main.fef808c3.js"
+  }
+]);
